339f36a828985133661c672b1d2d940346350655
Lisa Chikovore <lisachikovore@gmail.com>
2026-05-14 11:46:00 +0200

    Updated UI and fixed menu issues

---
+++ src/Main.kt
@@ -0,0 +1,16 @@
+//lesson 1
+
+//fun main(){
+//    println("Hello World!")
+//}
+
+//lesson variables
+
+fun main() {
+    var userName:String="Lisa"
+    userName="Daisy"
+    println("Hello $userName !")
+//    println("Hello Lisa!")
+
+
+}
